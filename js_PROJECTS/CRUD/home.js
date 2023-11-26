e49a73f7b34147a58b421afa9e05d8ca4e2dddb9
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-11-26 23:44:28 +0000

    working on user panel

--- js_PROJECTS/CRUD/home.js
+++ js_PROJECTS/CRUD/home.js
@@ -20,25 +20,49 @@ function moviesDisplay(arr){
 
         let card_details=document.createElement("div");
         card_details.classList.add("card_details");
+
         let movie_title=document.createElement("p");
         movie_title.classList.add("movie_title");
         movie_title.innerText=ele.title;
         card_details.appendChild(movie_title);
 
-
-        let rating=document.createElement("p");
+        let rating=document.createElement("div");
         rating.classList.add("rating");
-        rating.innerText="rating";
+
+        let rating_stars=document.createElement("div");
+        rating_stars.classList.add("rating_star");
+
+        let whitestars=document.createElement("div");
+        whitestars.classList.add("whitestars");
+        for(let c=0;c<5;c++){
+          let icon=document.createElement("i");
+          icon.classList.add("icon");
+          icon.classList.add("fa-solid");
+          icon.classList.add("fa-star");
+          whitestars.appendChild(icon);
+        }    
+        rating_stars.appendChild(whitestars);
+        rating.appendChild(rating_stars);
         card_details.appendChild(rating);
-    
+
+        let yellowstars=document.createElement("div");
+        yellowstars.classList.add("yellowstars");
+        for(let c=0;c<5;c++){
+          let icon=document.createElement("i");
+          icon.classList.add("icon");
+          icon.classList.add("fa-solid");
+          icon.classList.add("fa-star");
+          yellowstars.appendChild(icon);
+        }    
+        rating_stars.appendChild(yellowstars);
+        rating.appendChild(rating_stars);
+        card_details.appendChild(rating);
+
 
         let button=document.createElement("button");
         button.classList.add("button");
         button.innerText="DETAILS";
         card_details.appendChild(button);
-
-
-        
 
         card.appendChild(card_image);
         card.appendChild(card_details);
