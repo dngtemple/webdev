48573aac4bf11d2b1034ef6d60232dd1b488243f
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-12-26 16:35:29 +0000

    learning about creating api to check get method and products id as well

--- NODEjs/createAPI.js
+++ NODEjs/createAPI.js
@@ -1,23 +1,38 @@
 const http=require("http");
 const fs=require("fs");
+const url=require("url");
 
 http.createServer(function(req,res){
 
-    if(req.method==="GET" && req.url==="/products"){
-        let data= fs.readFileSync("./products.json",{encoding:"utf-8"});
-        res.write(data);
-    }
-    else if(req.method==="POST"){
-        res.write("This is post");
-    }
-    else if(req.method==="DELETE"){
-        res.write("This is delete method");
-    }
+    let parsedUrl=url.parse(req.url,true);
+    // console.log(parsedUrl);
+    if(req.method==="GET" && parsedUrl.pathname==="/products"){
 
+        let products=fs.readFileSync("./products.json",{encoding:"utf-8"});
+
+        let id=parsedUrl.query.id;
+        if(id===undefined){
+            res.write(products);
+        }
+        else{
+
+            let pro=JSON.parse(products);
+            let product=pro.find(function(prod,index){
+                return Number(prod.id)===Number(id);
+            })
+            if(product!==undefined){
+                res.write(JSON.stringify(product));
+            }
+            else{
+                res.write(JSON.stringify({message:"Invalid product ID"}));
+            }
+
+        }
+    }
     res.end();
 
   
 
 }).listen(8000,function(){
-    console.log("The server is running");
+    console.log("Running successfully");
 })
