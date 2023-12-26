58ed44b8b0627d739dbe7f3e9fa08ba98980ab7d
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-12-26 13:36:26 +0000

    creation of API and reading a file

--- NODEjs/createAPI.js
+++ NODEjs/createAPI.js
@@ -1,11 +1,22 @@
 const http=require("http");
+const fs=require("fs");
 
 http.createServer(function(req,res){
 
-    console.log(req.method);
+    if(req.method==="GET" && req.url==="/products"){
+        let data= fs.readFileSync("./products.json",{encoding:"utf-8"});
+        res.write(data);
+    }
+    else if(req.method==="POST"){
+        res.write("This is post");
+    }
+    else if(req.method==="DELETE"){
+        res.write("This is delete method");
+    }
 
-    res.write("Your response is working now");
     res.end();
+
+  
 
 }).listen(8000,function(){
     console.log("The server is running");
