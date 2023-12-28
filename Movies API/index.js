7ed40b629386f818abb097c17c9c7e73294c7cf1
Clinton Templeton <donnahaytempleton1230@gmail.com>
2023-12-28 20:20:41 +0000

    learning about express and middleware

--- Movies API/index.js
+++ Movies API/index.js
@@ -1,3 +1,5 @@
+// introduction to express
+
 const express=require("express");
 const app=(express());
 
@@ -5,10 +7,25 @@ app.get("/products",function(req,res){
     res.send("Get response coming in");
 })
 
-app.post("/products",function(req,res){
+app.post("/products",gateman,function(req,res){
+    console.log(req.headers);
     res.send("Post response coming in");
 })
 
 app.listen(8000,function(){
     console.log("Server is running");
 })
+
+
+
+// learning about middleware which is a gateman between client and endpoint in the server
+
+function gateman(req,res,pusher){
+
+    if(req.headers['content-type'] ===undefined){
+        res.send("Errors in the post headers");
+    }
+    else{
+        pusher();
+    }
+}
