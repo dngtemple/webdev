c35f2ea19803a7c4160e3696f9b4653f4235cd43
Clinton Templeton <donnahaytempleton1230@gmail.com>
2024-01-01 18:44:07 +0000

    clearing  code and learning about mongoose again import module and connect databaese create Schema, create model

--- NODEjs/db.mongoose.js
+++ NODEjs/db.mongoose.js
@@ -1,32 +1,44 @@
 const mongoose=require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/dummy')
+mongoose.connect("mongodb://127.0.0.1:27017/dummy")
 .then(function(msg){
-    console.log("connection successfull");
+    console.log("Successfully connected");
 })
 .catch(function(err){
-    console.log("error connecting",err)
+    console.log(err);
 })
-
 
 const teacherSchema=mongoose.Schema({
     name:String,
     dep:String,
-    sal:String
+    sal:String,
 })
-    
+
 const teacherModel=mongoose.model("teachers",teacherSchema);
 
-let data={
-    name:"Efo",
-    dep:"Accounting",
-    sal:"4440"
-}
+
+let data=[
+    {
+        name:"Efia",
+        dep:"Accounting",
+        sal:"4589"
+    },
+    {
+        name:"Kofi",
+        dep:"Sales",
+        sal:"112"
+    },
+    {
+        name:"James",
+        dep:"Development",
+        sal:"2253"
+    }
+]
 
 teacherModel.create(data)
-.then(function(msg){
-    console.log("successfully",msg)
+.then(function(){
+    console.log("Data successfully Added");
 })
 .catch(function(err){
-    console.log(err);
+    console.log("Data couldn't be added")
 })
