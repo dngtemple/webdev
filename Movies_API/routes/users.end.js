a298d5b4eecb039c46910608cbec748a49368e8e
Clinton Templeton <donnahaytempleton1230@gmail.com>
2024-01-05 18:57:21 +0000

    learning about jsonwebtoken to generate tokens

--- Movies_API/routes/users.end.js
+++ Movies_API/routes/users.end.js
@@ -1,12 +1,12 @@
 const userModel=require("../models_sch/users.model");
 
 const express=require("express");
-const { use } = require("./movies_end");
 const app=express();
 app.use(express.json());
 const router=express.Router();
 
 const bcryptjs=require("bcryptjs");
+const jsonwebtoken=require("jsonwebtoken");
 
 
 // endpoint of post and hashing password
@@ -50,7 +50,17 @@ router.post("/login",function(req,res){
             bcryptjs.compare(userCredentials.password,user.password,function(err,result){
                 if(err=== null || err=== undefined){
                     if(result){
-                        res.send({message:"login successful"});
+
+                        // username:userCredentials.user----this is something unique of the user
+                            // since they are login in username is best
+
+                        jsonwebtoken.sign({username:userCredentials.user},"secretkey",function(err,token){
+                            if(err===null || err===undefined){
+                                res.send({success:true,token:token});
+                            }
+                        })
+
+                        // res.send({message:"login successful"});
                     }
                     else{
                         res.send({message:"wrong password"});
@@ -64,5 +74,34 @@ router.post("/login",function(req,res){
         console.log(err);
     })
 })
+
+
+router.get("/something",verify,function(req,res){
+
+    res.send({message:"I am the most secured endpoint"});
+})
+
+
+// middleware to vertify token
+function verify(req,res,next){
+
+    if(req.headers.authorization !==undefined){
+
+        let token=req.headers.authorization.split(" ")[1];
+
+        jsonwebtoken.verify(token,"secretkey",function(err,data){
+            if(err===null || err===undefined){
+                next();
+            }
+            else{
+                res.send({message:"Invalid token"});
+            }
+        })
+
+    }
+    else{
+        res.send({message:"Please add token"});
+    }
+}
 
 module.exports=router;
