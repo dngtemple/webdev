0152e87afde4717849ffa4def32afbfd99d32109
Clinton Templeton <donnahaytempleton1230@gmail.com>
2024-01-11 00:18:50 +0000

    learning about HOOKS useState,useRef,useEffect in react

--- React/firstapp/src/demo.js
+++ React/firstapp/src/demo.js
@@ -1,26 +1,45 @@
-import { useState } from "react";
+import { useState,useRef, useEffect } from "react";
 
 
 function Demo(){
 
-    // let name="Jeffery";
-
+    // using useState variable          // with state the page re renders
     let[name,setName]=useState("John");
-
     function ChangeName(){
         // name="Eric";
-
         setName("Kofi");
     }
-    console.log(name);
+
+
+    // using useRef variable            // with ref the page does re render
+    let user=useRef("User_1");
+    // console.log(user.current);
+    function ChangeUser(){
+        user.current="User_2";
+    }
+
+
+
+    // using useEffect
+
+    useEffect(function(){
+        console.log("Thie is something that renders and re renders");
+    },[])
 
     return(
         <div className="demo">
-           <h1>{name}</h1>
 
+           <h1>{name}</h1>
            <button onClick={function(){
               ChangeName();
            }}>Change name</button>
+
+
+           <h2>{user.current}</h2>
+
+           <button onClick={function(){
+              ChangeUser();
+           }}>ChangeUser</button>
 
         </div>
     );
