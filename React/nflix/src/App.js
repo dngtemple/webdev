c835a7291e9dba3e3ef01034d6dc3923aab06ad9
Clinton Templeton <donnahaytempleton1230@gmail.com>
2024-01-23 02:18:53 +0000

    starting the frontend project for the movies api in React( the nflix project)

---
+++ React/nflix/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+
+// route
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ React/nflix/src/components/Homepage.js
@@ -0,0 +1,6 @@
+function Homepage(){
+    return(
+        <h3>Homepage</h3>
+    );
+}
+export default Homepage;
